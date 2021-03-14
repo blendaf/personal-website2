c9b3490c2610563898627f90ef4002ba2fc8e178
Blenda Fröjdh <blendaf@kth.se>
2021-03-14 20:49:12 +0100

    fix: correctly import fonts

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,6 @@ import Head from 'next/head'
 
 const GlobalStyle = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
 
 
   body {
@@ -103,6 +101,11 @@ export default function App({ Component, pageProps }) {
           type="image/png"
           sizes="16x16"
           href="/favicon-16x16.png"
+        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Quicksand:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
