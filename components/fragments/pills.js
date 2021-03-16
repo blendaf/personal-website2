ba05682cb5d0ab2c2e88f26d137a15e52f3e6818
Blenda Fröjdh <blendaf@kth.se>
2021-03-16 20:40:42 +0100

    Minor fix: style pills

--- components/fragments/pills.js
+++ components/fragments/pills.js
@@ -8,13 +8,12 @@ const PillList = styled.ul`
 `
 
 const Pill = styled.li`
-  padding: 5px 8px;
+  padding: 5px 10px;
   background-color: ${({ theme }) => theme.colors.hoverBlue};
   color: ${({ theme }) => theme.colors.white};
   font-size: ${({ theme }) => theme.fontSize.signsNormal};
   font-weight: 700;
   border-radius: 20px;
-  /* border: 1px solid ${({ theme }) => theme.colors.black}; */
   list-style-type: none;
   margin-right: 5px;
   margin-top: 8px;
